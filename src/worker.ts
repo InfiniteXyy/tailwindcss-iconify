be9e45a4cbb760f52f0ec3f5ed49e6e6de90ea47
infiniteXyy <xuyiyangwing@gmail.com>
2022-05-02 18:06:11 +0800

    fix: retry load methods when collection name is multi world

--- src/worker.ts
+++ src/worker.ts
@@ -3,15 +3,21 @@ import { loadNodeIcon } from '@iconify/utils/lib/loader/node-loader'
 // Tailwind does not support async plugins
 // So my workaround is to load icon in a worker process with spawnSync
 
+const COLLECTION_NAME_PARTS_MAX = 3
+
 async function loadIconFromValue(value: string) {
-  const [_value, collection, name] = value.match(/^(.*?)-(.*)$/) || []
-  return await loadNodeIcon(collection, name, {
-    addXmlNs: true,
-    warn: undefined,
-    customizations: {
-      trimCustomSvg: true,
-    },
-  })
+  const parts = value.split('-')
+  for (let i = 0; i < COLLECTION_NAME_PARTS_MAX; i++) {
+    const [collection, name] = [parts.slice(0, i).join('-'), parts.slice(i).join('-')]
+    const svg = await loadNodeIcon(collection, name, {
+      addXmlNs: true,
+      warn: undefined,
+      customizations: {
+        trimCustomSvg: true,
+      },
+    })
+    if (svg) return svg
+  }
 }
 
 const [iconName] = process.argv.slice(2)
